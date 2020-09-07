d7da823259ea238230926b7d1e1d6003866ea29d
Jordan Hubbard <jordanhubbard@gmail.com>
2020-09-07 14:49:43 -0700

    Add a little helper script for starting remote jupyter notebook server.

---
+++ bin/open-notebook.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+HOST=megamind.local
+FILE=/tmp/fump.$$
+( sleep 5; open `egrep '^[\t ]+http://' ${FILE}`; rm ${FILE} ) &
+ssh jkh@${HOST} jupyter notebook --no-browser --ip=0.0.0.0 2>&1 |tee ${FILE}
