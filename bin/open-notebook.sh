28241ce00274c2050b83160c9d81b1310028f129
Jordan Hubbard <jordanhubbard@gmail.com>
2020-09-07 15:44:33 -0700

    Try to put jupyter in the right directory by default.

--- bin/open-notebook.sh
+++ bin/open-notebook.sh
@@ -2,4 +2,4 @@
 HOST=megamind.local
 FILE=/tmp/fump.$$
 ( sleep 5; open `egrep '^[\t ]+http://' ${FILE}`; rm ${FILE} ) &
-ssh jkh@${HOST} jupyter notebook --no-browser --ip=0.0.0.0 2>&1 |tee ${FILE}
+ssh jkh@${HOST} '(cd Src/Notebooks; jupyter notebook --no-browser --ip=0.0.0.0 )' 2>&1 |tee ${FILE}
