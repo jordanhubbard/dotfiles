9285cea2ebc84d74a2f44ac18ee32ace6cbe7726
Jordan Hubbard <jordanhubbard@gmail.com>
2020-10-28 11:31:37 -0700

    Use proper startup directory or container volume

--- bin/jupyter-start.sh
+++ bin/jupyter-start.sh
@@ -8,6 +8,10 @@ fi
 # Increase the JIT cache size for Ampere
 export CUDA_CACHE_MAXSIZE=2147483648
 
+# Where we'd like to start the notebook server if possible.
+DEFHOME=$HOME/Src/Notebooks
+[ -d $DEFHOME ] || DEFHOME=$HOME
+
 if [ "$1" = "-d" ]; then
 	shift
 	if [ "$1" = "-r" ]; then
@@ -18,7 +22,8 @@ if [ "$1" = "-d" ]; then
 		R=""
 	fi
 
-	docker run --gpus all $USER -v /home/jkh/Src/Notebooks:/workspace/Notebooks -it -p 8888:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 jkh-tf2
+	docker run --gpus all $USER -v $DEFHOME:/workspace/Notebooks -it -p 8888:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 jkh-tf2
 else
+	cd $DEFHOME
 	jupyter notebook --no-browser --ip=0.0.0.0
 fi
