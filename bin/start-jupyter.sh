0104afca6b2c49cf59990c588d158d4409d3ac25
Jordan Hubbard <jordanhubbard@gmail.com>
2021-10-19 11:32:07 -0700

    Add some comments and streamline the script a bit.

--- bin/start-jupyter.sh
+++ bin/start-jupyter.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 if [ "$1" = "-h" ]; then
-	echo "$0: [-d] to run containerized or bare metal"
+	echo "$0: [-d] to run containerized or no args for bare metal"
 	exit 1
 fi
 
-# Increase the JIT cache size for Ampere
+# Increase the JIT cache size for Ampere because otherwise EXPLODE
 export CUDA_CACHE_MAXSIZE=2147483648
 export CONTAINER=nvcr.io/nvidia/tensorflow:21.09-tf1-py3
+export JUPYTER_CMD="jupyter notebook --no-browser --ip=0.0.0.0"
+export DEFHOME=$HOME/Src/Notebooks
 
-# Where we'd like to start the notebook server if possible.
-DEFHOME=$HOME/Src/Notebooks
+# Start the notebook server in my notebooks directory, if possible.
 [ -d $DEFHOME ] || DEFHOME=$HOME
 
+# If running containerized, some additional args need to be parsed.
 if [ "$1" = "-d" ]; then
 	shift
 	if [ "$1" = "-r" ]; then
@@ -23,8 +25,9 @@ if [ "$1" = "-d" ]; then
 		R=""
 	fi
 
-	docker run --gpus all $USER -v $DEFHOME:/workspace/Notebooks -it -p 8888:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 ${CONTAINER} jupyter notebook --no-browser --ip=0.0.0.0
+	docker run --gpus all $USER -v $DEFHOME:/workspace/Notebooks -it -p 8888:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 ${CONTAINER} ${JUPYTER_CMD}
 else
+	# Simple bare-metal case here - just run the jupyter command
 	cd $DEFHOME
-	jupyter notebook --no-browser --ip=0.0.0.0
+	${JUPYTER_CMD}
 fi
