214faf6ea6a514c1aaf6d0765e0760b5371beee1
Jordan Hubbard <jordanhubbard@gmail.com>
2021-11-01 13:23:52 -0700

    Make vpn script work for linux too.

--- bin/vpn.sh
+++ bin/vpn.sh
@@ -114,7 +114,12 @@ fi
 
 [ -z "${_USER}" ] && _USER=${DEF_USER}
 
-_VPN_ARGS="-s 'vpn-slice --verbose --dump -I -i --domains-vpn-dns=nvidia.com,nvmetal.net %10.11.0.0/16 10.0.0.0/8 72.25.64.0/18 216.228.112.0/20 209.66.87.0/24 24.51.0.0/19 64.125.39.0/24 mail wiki confluence gpuwa nvinfo nvbugs nvbugswb prestige hqnvwa11 hqnvwa12 ssogate nvsso ssoauth teams dlrequest p4protects coupa vpn.nvidia.com apps.nvinfo.nvidia.com pid pdp services gitlab-master sapdctabl1 sape7psys sape7pscs docusign prom nv nvsearch psgview p4viewer view prod.vault.nvidia.com ngc' --authgroup=Employee -u ${_USER}"
+# vpn-slice doesn't work great on my linux boxes - elide it.
+if isOSNamed Linux; then
+   _VPN_ARGS="--authgroup=Employee -u ${_USER}"
+else
+   _VPN_ARGS="-s 'vpn-slice --verbose --dump -I -i --domains-vpn-dns=nvidia.com,nvmetal.net %10.11.0.0/16 10.0.0.0/8 72.25.64.0/18 216.228.112.0/20 209.66.87.0/24 24.51.0.0/19 64.125.39.0/24 mail wiki confluence gpuwa nvinfo nvbugs nvbugswb prestige hqnvwa11 hqnvwa12 ssogate nvsso ssoauth teams dlrequest p4protects coupa vpn.nvidia.com apps.nvinfo.nvidia.com pid pdp services gitlab-master sapdctabl1 sape7psys sape7pscs docusign prom nv nvsearch psgview p4viewer view prod.vault.nvidia.com ngc' --authgroup=Employee -u ${_USER}"
+fi
 
 echo "Connecting to VPN server ${_VPN} - you will need to supply your password"
 eval sudo openconnect ${_VPN} ${_VPN_ARGS}
