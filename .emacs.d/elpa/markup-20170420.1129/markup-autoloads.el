af1356d60bc12c25e8c890ea7ece7dbaef317d61
Jordan Hubbard <jordanhubbard@gmail.com>
2023-03-28 22:33:30 -0700

    Get rid of the cached emacs directory - not needed anymore.

--- .emacs.d/elpa/markup-20170420.1129/markup-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; markup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "markup" "markup.el" (0 0 0 0))
-;;; Generated autoloads from markup.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "markup" '("map-group-if" "markup" "*markup-" "ensure-string" "keyword-name")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; markup-autoloads.el ends here
