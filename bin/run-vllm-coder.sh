0e8be4792046e7316105f1ff990237c1a8f5085c
Jordan Hubbard <jordanhubbard@gmail.com>
2026-02-09 11:15:05 -0800

    Report which model is being used

--- bin/run-vllm-coder.sh
+++ bin/run-vllm-coder.sh
@@ -6,6 +6,8 @@ else
 	MODEL_SIZE="$*"
 fi
 
+echo "Using model size of ${MODEL_SIZE}"
+
 case "${MODEL_SIZE}" in
 	small)
 	docker run -it --gpus all -p 8000:8000   --ipc=host --ulimit memlock=-1 --ulimit stack=67108864   -v ~/.cache/huggingface:/root/.cache/huggingface   nvcr.io/nvidia/vllm:25.12.post1-py3   vllm serve nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-FP8     --trust-remote-code 2>&1 | tee run-vllm-coder.out
