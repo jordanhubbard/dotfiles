9e6e50eb960f0d5cea14a8bc8c7e6fc50bd6bc78
Jordan Hubbard <jordanhubbard@gmail.com>
2026-02-09 10:10:25 -0800

    Add support for different model sizes

--- bin/run-vllm-coder.sh
+++ bin/run-vllm-coder.sh
@@ -1,1 +1,36 @@
-docker run -it --gpus all -p 8000:8000   --ipc=host --ulimit memlock=-1 --ulimit stack=67108864   -v ~/.cache/huggingface:/root/.cache/huggingface   nvcr.io/nvidia/vllm:25.12.post1-py3   vllm serve nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-FP8     --trust-remote-code 2>&1 | tee run-vllm-coder.out
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+	MODEL_SIZE="medium"
+else
+	MODEL_SIZE="$*"
+fi
+
+case "${MODEL_SIZE}" in
+	small)
+	docker run -it --gpus all -p 8000:8000   --ipc=host --ulimit memlock=-1 --ulimit stack=67108864   -v ~/.cache/huggingface:/root/.cache/huggingface   nvcr.io/nvidia/vllm:25.12.post1-py3   vllm serve nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-FP8     --trust-remote-code 2>&1 | tee run-vllm-coder.out
+	;;
+
+	medium)
+	docker run -it --gpus all -p 8000:8000 \
+    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+    -v ~/.cache/huggingface:/root/.cache/huggingface \
+    nvcr.io/nvidia/vllm:25.12.post1-py3 \
+    vllm serve Qwen/Qwen2.5-Coder-32B-Instruct \
+      --trust-remote-code \
+      --max-model-len 65536 \
+      --tensor-parallel-size $(nvidia-smi -L | wc -l) \
+    2>&1 | tee run-vllm-coder.out
+	;;
+
+	large)
+	echo "large not supported yet"
+	exit 1
+	;;
+
+	*)
+	echo "unknown model size ${MODEL_SIZE}"
+	exit 1
+	;;
+esac
+
